e2a690a140ae17bbfd2ad7d8096431d1b1773873
Ezequiel massa <ezequielmassa.dev@gmail.com>
2023-03-31 18:09:58 -0300

    organized imports

--- src/main/java/com/emdev/wallet/controller/MovementsController.java
+++ src/main/java/com/emdev/wallet/controller/MovementsController.java
@@ -1,14 +1,17 @@
 package com.emdev.wallet.controller;
 
-import com.emdev.wallet.model.Deposit;
-import com.emdev.wallet.model.Movements;
-import com.emdev.wallet.service.IMovementsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.emdev.wallet.model.Movements;
+import com.emdev.wallet.service.IMovementsService;
 
 @RestController
 @RequestMapping("/api/v1/movements")
@@ -19,7 +22,7 @@ public class MovementsController {
     private IMovementsService movementsService;
 
     @GetMapping("/id/{id}")
-    public ResponseEntity<List<Movements>> getAllAccountMovements(@PathVariable("id") Integer id){
+    public ResponseEntity<List<Movements>> getAllAccountMovements(@PathVariable("id") Integer id) {
         List<Movements> accountMovements = movementsService.getAccountMovements(id);
         return ResponseEntity.ok(accountMovements);
     }
--- src/main/java/com/emdev/wallet/service/TransferService.java
+++ src/main/java/com/emdev/wallet/service/TransferService.java
@@ -1,14 +1,13 @@
 package com.emdev.wallet.service;
 
-import com.emdev.wallet.model.Deposit;
-import com.emdev.wallet.model.Transfer;
-
 import java.util.List;
 
+import com.emdev.wallet.model.Transfer;
 
 public interface TransferService {
     List<Transfer> getAccountTransfers(Integer accountId);
+
     Transfer getTransfer(Integer transferId);
 
-    Transfer createTransfer(Integer accountOriginId,Integer accountDestinyId, Transfer transfer) throws Exception;
+    Transfer createTransfer(Integer accountOriginId, Integer accountDestinyId, Transfer transfer) throws Exception;
 }
